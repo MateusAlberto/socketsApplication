8ef294ca92fd1bbe63a0df5e9b7a1a2503532305
MateusAlberto <madof007@gmail.com>
2020-10-31 13:18:40 -0300

    Início comunicação udp

--- sistemaDePrecos/cliente/cliente.go
+++ sistemaDePrecos/cliente/cliente.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+const tamanhoMaxMensagem = 512
 
 func main() {
-	fmt.Println("Hello World!")
+	enderecoEPorta := os.Args[1]
+
+	enderecoServidor, err := net.ResolveUDPAddr("udp", enderecoEPorta)
+	if err != nil {
+		fmt.Println("Endereço incorreto:", err)
+		os.Exit(-1)
+	}
+
+	fmt.Println("Conectando no servidor", enderecoEPorta)
+	//Conectando no endereço e porta especificados (pelo padrão endereço:porta)
+	socket, err := net.DialUDP("udp", nil, enderecoServidor)
+	if err != nil {
+		fmt.Println("Ocorreu um erro ao tentar se conectar ao servidor:", err)
+		os.Exit(-1)
+	}
+	fmt.Println("Conexão estabelecida com", enderecoEPorta)
+	fmt.Println("Endereço do servidor:", socket.RemoteAddr().String())
+	fmt.Println("Endereço local do cliente:", socket.LocalAddr().String())
+
+	defer socket.Close()
+
+	for {
+		// write a message to server
+		johnLennon := bufio.NewReader(os.Stdin)
+		mensagem, err := johnLennon.ReadString('\n')
+		if err == io.EOF {
+			return //se deu EOF na leitura padrão, é porque o programa cliente foi fechado
+		}
+
+		_, err = socket.Write([]byte(mensagem))
+
+		if err != nil {
+			fmt.Println("Erro ao tentar escrever uma mensagem para o servidor:", err)
+		}
+
+		//recebendo uma mensagem do servidor
+		buffer := make([]byte, tamanhoMaxMensagem)
+		tamMensagem, endereco, err := socket.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println("Erro ao tentar ler uma mensagem do servidor:", err)
+		}
+
+		fmt.Println("Servidor UDP:", endereco)
+		fmt.Println("Recebido do servidor UDP:", string(buffer[:tamMensagem]))
+	}
 }
--- sistemaDePrecos/servidor/servidor.go
+++ sistemaDePrecos/servidor/servidor.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+const tamanhoMaxMensagem = 512
 
 func main() {
-	fmt.Println("Hello World!")
+	porta := os.Args[1]
+
+	enderecoUDP, err := net.ResolveUDPAddr("udp", "localhost:"+porta)
+	if err != nil {
+		fmt.Println("Endereço incorreto:", err)
+		os.Exit(-1)
+	}
+
+	socket, err := net.ListenUDP("udp", enderecoUDP)
+	if err != nil {
+		fmt.Println("Ocorreu um erro ao ouvir a porta:", err)
+		os.Exit(-1)
+	}
+	fmt.Println("Servidor ouvindo na porta", porta)
+	defer socket.Close() //vai garantir que irá fechar o listener assim que fechar o programa
+
+	for {
+		buffer := make([]byte, tamanhoMaxMensagem)
+
+		tamMensagem, endereco, err := socket.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println("Erro ao ler mensagem do cliente:", err)
+		}
+
+		fmt.Println("Cliente : ", endereco)
+		fmt.Println("Recebido do cliente:", string(buffer[:tamMensagem]))
+
+		mensagem := []byte((buffer[:tamMensagem]))
+		_, err = socket.WriteToUDP(mensagem, endereco)
+		if err != nil {
+			fmt.Println("Erro ao tentar enviar uma mensagem para o cliente:", err)
+		}
+	}
 }
